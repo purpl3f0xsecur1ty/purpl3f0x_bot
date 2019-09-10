b261c94c27385bb56d4dc36a6b14508e1c65c8d3
purpl3-f0x <42351007+purpl3-f0x@users.noreply.github.com>
2019-09-10 17:54:01 -0400

    Update purpl3f0x_bot.py

--- purpl3f0x_bot.py
+++ purpl3f0x_bot.py
@@ -3,6 +3,9 @@
 import discord
 import hashlib
 import base64
+import random
+import time
+
 
 from discord.ext import commands
 
@@ -22,7 +25,42 @@ async def on_ready():
     print(discord.__version__)
     for server in client.servers:  # List connected servers
         print('Joined: {}'.format(server.name))
-        print(server.default_channel)
+
+choices = [
+        "💜**Definitely!**💜",
+        "🦊**Maybe**🦊",
+        "🚫**No**🚫",
+        "❔**I don't know**🦊",
+        "⛔**Never**⛔",
+        "🔥**Hell no**🛑",
+        "🦊**Fuck yea!**🦊",
+        "🕳*Fox stuck in hole, try again later*🦊",
+        "👍**Very likely~**🦊",
+        "🛑**I don't think so**🛑"
+]
+
+# Array for fact command
+facts = [
+        "Foxes are without a doubt the sneakiest little fuckers in the entire animal kingdom.",
+        "Foxes skulk around doing all kinds of dodgy shit far more than any other species.",
+        "Foxes are quick to recognize lewdness.",
+        "A female fox is called a vixen, and a male fox is called a dog fox or a tod.",
+        "Foxes are loners except during mating season.",
+        "Like cats, foxes are most active at night, and their eyes narrow into vertical slits.",
+        "Foxes harness the earth's magnetic field to hunt.",
+        "Foxes are good parents, caring for their pups for 7 months and going to great lengths to protect them.",
+        "Having pet foxes is legal in certain states, some of which require permits.",
+        "Arctic foxes don't feel cold until -94F/-70C.",
+        "Foxes make 40 different sounds, the most startling of which is its scream.",
+        "Foxes have understood the universe for many years.",
+        "Foxes eat nothing but grapes.🍇",
+        "Foxes will inherit the earth after puny fur-less humans die off in the next ice age.",
+        "Foxes are not amused by 'What Does The Fox Say' so don't make that joke around them.",
+        "Foxes are prone to pinching the booty.",
+        "Foxes will drink your pepsi and call you a bitch.",
+        "Foxes fancy the color purple the most."
+]
+
 
 # Commands start here
 # Basic info command
@@ -45,7 +83,10 @@ async def commands(ctx):
                     "`f.sha1 <text>` - Calculate a SHA1 hash\n"
                     "`f.sha256 <text>` - Calculate a SHA256 hash\n"
                     "`f.b64encode <text>` - Encode text with Base64\n"
-                    "`f.b64decode <b64 encoded text>` - Decode Base64"
+                    "`f.b64decode <b64 encoded text>` - Decode Base64\n"
+                    "**Fun:**\n"
+                    "`f.fact` - Random fox facts!\n"
+                    "`f.magicball` - Ask the fox a question!"
                     )
 
 # Begin cryptography commands
@@ -88,6 +129,22 @@ async def b64decode(ctx):
 
     await client.say(base64.b64decode(message).decode('utf-8'))
 
+# Fun commands
+
+@client.command(pass_context=True)
+async def fact(ctx):
+    await client.say(random.choice(facts))
+
+@client.command(pass_context=True)
+async def magicball(ctx):
+    await client.say("✨*Shaking magic tail...*✨")
+    time.sleep(1)
+    await client.say("🦊*Wiggling nose...*🦊")
+    time.sleep(1)
+    await client.say("🖥*Decrypting answer...*🖥")
+    time.sleep(2)
+    await client.say(random.choice(choices))
+
 # Just for fun:
 # Checks for a string and reacts
 @client.event
@@ -95,8 +152,9 @@ async def on_message(message):
 
     if "fox" in message.content.lower():
         await client.add_reaction(message, "🦊")
-    await client.process_commands(message) # Without this command the bot will not carry out any commands!!
+    await client.process_commands(message) # Without this command, no other commands will be processed!
+
 
 # Login token
 # Note that this line must come LAST
-client.run('YOUR TOKEN HERE')
+client.run('NDg2MjI4MzE1NDY0Nzk0MTEy.Dm8DQQ.ksDSeK-7JpxClXg3C-aCTkE4s9o')
